9204cf33c0121b1302c62383cab7b8dcaafa9c15
alanpilloud <alanpilloud@gmail.com>
2015-12-27 22:06:44 +0100

    Initial commit

---
+++ bwap-mail.js
@@ -0,0 +1,112 @@
+var form = document.getElementById('mailform'),
+    formMsg = document.getElementById('form-msg'),
+    requiredFields = form.getElementsByClassName('required');
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+
+    formMsg.style.display = 'none';
+    formMsg.className = 'alert';
+
+    var errorOccured = false;
+    for (var i = requiredFields.length-1; i >= 0; i--) {
+        that = requiredFields[i];
+        parent = that.parentElement;
+        if (that.value === '') {
+            parent.classList.add('has-error');
+            errorOccured = true;
+        } else {
+            if(parent.classList.contains('has-error')) {
+                parent.classList.remove('has-error');
+            }
+        }
+    }
+
+    if(errorOccured === true) {
+        formMsg.innerHTML = formMsg.getAttribute('data-required');
+        formMsg.style.display = 'block';
+        formMsg.className = 'alert alert-danger';
+        //return false;
+    }
+
+    var xmlhttp;
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function(){
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+            console.log(xmlhttp.responseText);
+            response = JSON.parse(xmlhttp.responseText);
+            if (response.status === 1) {
+                formMsg.innerHTML = response.msg;
+                formMsg.style.display = 'block';
+                formMsg.className = 'alert alert-success';
+            } else {
+                formMsg.innerHTML = response.msg;
+                formMsg.style.display = 'block';
+                formMsg.className = 'alert alert-danger';
+            }
+        }
+    }
+    xmlhttp.open("POST", '/wp-admin/admin-ajax.php?action=sendform', true);
+    xmlhttp.send(serialize(form));
+
+    return false;
+});
+
+function serialize(form) {
+    var i,
+        j,
+        q = [],
+        fd = new FormData();
+
+    for (i = form.elements.length - 1; i >= 0; i = i - 1) {
+        if (form.elements[i].name === "") {
+            continue;
+        }
+        switch (form.elements[i].nodeName) {
+        case 'INPUT':
+            switch (form.elements[i].type) {
+                case 'text':
+                case 'hidden':
+                case 'email':
+                case 'number':
+                case 'password':
+                    //q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
+                    fd.append(form.elements[i].name, form.elements[i].value);
+                    break;
+                case 'checkbox':
+                case 'radio':
+                    if (form.elements[i].checked) {
+                        //q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
+                        fd.append(form.elements[i].name, form.elements[i].value);
+                    }
+                    break;
+                case 'file':
+                    fd.append(form.elements[i].name, form.elements[i].files[0])
+                    break;
+            }
+            break;
+        case 'TEXTAREA':
+            //q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
+            fd.append(form.elements[i].name, form.elements[i].value);
+            break;
+        case 'SELECT':
+            switch (form.elements[i].type) {
+            case 'select-one':
+                //q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
+                fd.append(form.elements[i].name, form.elements[i].value);
+                break;
+            case 'select-multiple':
+                for (j = form.elements[i].options.length - 1; j >= 0; j = j - 1) {
+                    if (form.elements[i].options[j].selected) {
+                        //q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].options[j].value));
+                        fd.append(form.elements[i].name, form.elements[i].options[j].value);
+                    }
+                }
+                break;
+            }
+            break;
+        }
+    }
+    return fd;
+    //return q.join("&");
+}
---
+++ bwap-mail.php
@@ -0,0 +1,109 @@
+<?php
+/*
+Plugin Name: BWAP Mail
+Plugin URI: http://alanpilloud.github.io
+Description: Add a simple way to use your bootstrap form with ajax
+Version: 0.1
+Author: Bureau Web Alan Pilloud
+Author URI: http://alanpilloud.github.io
+*/
+
+defined( 'ABSPATH' ) or die;
+
+require 'plugin_update_check.php';
+$MyUpdateChecker = new PluginUpdateChecker_2_0 ('https://kernl.us/api/v1/updates/5680505a086e629607788b83/', __FILE__, 'bwap-mail', 1);
+
+if (!class_exists('BwapMail')) {
+
+    add_action( 'wp_ajax_sendform', array( 'BwapMail', 'sendform_callback') );
+    add_action( 'wp_ajax_nopriv_sendform', array( 'BwapMail', 'sendform_callback') );
+
+    class BwapMail
+    {
+        protected $tag = 'bwap-mail';
+        protected $name = 'BWAP Mail';
+        protected $version = '0.1';
+
+        public function __construct()
+        {
+            add_shortcode( $this->tag, array( &$this, 'shortcode' ) );
+        }
+
+        public function shortcode( $atts, $content = null )
+        {
+            $this->enqueue();
+            
+            return '<div id="form-msg" style="display:none" data-error="Une erreur a eu lieu pendant l\'envoi de votre message" data-required="Certains champs n\'ont pas été remplis."></div>';
+        }
+
+        protected function enqueue()
+        {
+            $plugin_path = plugin_dir_url( __FILE__ );
+            wp_enqueue_script($this->tag, $plugin_path . 'script.js', null, $this->version, true);
+        }
+
+        public function sendform_callback()
+        {
+            $return = array(
+                'status'=>1,
+                'msg'=> 'Votre message a bien été envoyé. Nous y répondrons au plus vite.',
+            );
+            $html = array();
+            $data = filter_input_array(INPUT_POST);
+
+            $info = pathinfo($_FILES['userFile']['name']);
+            $ext = $info['extension']; // get the extension of the file
+            $newname = "newname.".$ext;
+
+            $target = 'images/'.$newname;
+
+            if (filter_var($data['email'], FILTER_VALIDATE_EMAIL) == false) {
+                $return['status'] = 0;
+                $return['msg'] = __('L\'adresse email indiquée n\'est pas valide.','bwap-mail');
+            }
+
+            if (count($_FILES) > 0) {
+                $upload_dir = wp_upload_dir();
+                $uploadpath = $upload_dir['basedir'].DIRECTORY_SEPARATOR.'bwap-mail';
+                if (!is_dir($uploadpath)) {
+                    mkdir($uploadpath);
+                }
+
+                foreach ($_FILES as $k => $file) {
+                    if ($file['error'] == 0) {
+                        $filename = uniqid().'-'.$file['name'];
+                        $filepath = $uploadpath.DIRECTORY_SEPARATOR.$filename;
+                        $moved = move_uploaded_file($file['tmp_name'], str_replace('/',DIRECTORY_SEPARATOR,$filepath));
+                        if (!$moved) {
+                            $return['status'] = 0;
+                            $return['msg'] = __('Le fichier n\'a pas pu être envoyé. Veuillez essayer à nouveau.','bwap-mail');
+                        } else {
+                            $html[] = '<b>'.str_replace('_',' ',$k).' :</b> '.$upload_dir['baseurl'].'/bwap-mail/'.$filename;
+                        }
+                    }
+                }
+            }
+
+            if ($return['status'] == 1) {
+                foreach($data as $k => $v) {
+                    if (!empty($v)) {
+                        $html[] = '<b>'.str_replace('_',' ',$k).' :</b> '.nl2br(stripslashes($v));
+                    }
+                }
+
+                $send = wp_mail(get_option('admin_email'), 'Contact du site', implode('<br/>',$html), array(
+                    'From: '.get_option('blogname').' <'.get_option('admin_email').'>',
+                    'Reply-to: '.$data['email'],
+                    'Content-type: text/html; charset=UTF-8'
+                ));
+
+                if ($send === false) {
+                    $return['status'] = 0;
+                    $return['msg'] = __('Il y a eu une erreur lors de l\'envoi. Merci de bien vouloir essayer à nouveau.<br/>'.implode('<br/>',$html),'bwap-mail');
+                }
+            }
+            die(json_encode($return));
+        }
+    }
+    new BwapMail;
+}
